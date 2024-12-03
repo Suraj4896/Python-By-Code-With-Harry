2e00688dad4d72d26ef64a430620a83c2365e490
suraj <surajkiranpanda98@gmail.com>
2024-12-03 23:20:49 +0530

    commited

---
+++ chapter6ps/q4.py
@@ -0,0 +1,6 @@
+username = input("Enter username: ");
+
+if(len(username) >= 10):
+    print("Not an username");
+else:
+    print("This is a valid username");
---
+++ chapter6ps/q5.py
@@ -0,0 +1,8 @@
+l1 = ["suraj", "rupa", "butu"];
+
+name = input("Enter a name: ");
+
+if(name in l1):
+    print("Yes it is present");
+else:
+    print("not present");
