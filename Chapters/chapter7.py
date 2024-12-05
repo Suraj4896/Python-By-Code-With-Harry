97442febaff288868105a8be110eb99f33f9d3e6
suraj <surajkiranpanda98@gmail.com>
2024-12-05 22:50:50 +0530

    commited

---
+++ Chapters/chapter7.py
@@ -0,0 +1,18 @@
+#while loop
+
+i = 0;
+while(i < 5):
+    print(i);
+    i += 1;
+
+#for loop
+
+name = ["suraj", "rupa", "kanha", "butu"];
+for i in name:
+    print(i);
+
+j = 0;
+while(j < len(name)):
+    print(name[j]);
+    j += 1;
+
