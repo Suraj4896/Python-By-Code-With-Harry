bd40d3f8d2a72767159ae133d5004d69fcdf7f92
suraj <surajkiranpanda98@gmail.com>
2024-12-06 12:09:13 +0530

    commited

--- Chapters/chapter7.py
+++ Chapters/chapter7.py
@@ -1,12 +1,12 @@
 #while loop
-
 i = 0;
 while(i < 5):
     print(i);
     i += 1;
 
-#for loop
 
+
+#for loop
 name = ["suraj", "rupa", "kanha", "butu"];
 for i in name:
     print(i);
@@ -16,3 +16,42 @@ while(j < len(name)):
     print(name[j]);
     j += 1;
 
+
+#range function
+for i in range(8):
+    print(i);
+
+#steps after increment of 4
+for i in range(0, 50, 4):
+    print(i);
+
+#for loop in tuple
+t = (23, 45, 67,89);
+for i in t:
+    print(i);
+
+#for with else
+l= [1,7,8]
+for item in l:
+    print(item)
+else:
+    print("done") 
+
+
+#break
+for i in range (0,80):
+    print(i) # this will print 0,1,2 and 3
+    if i==3:
+        break
+
+#continue
+for i in range(4):
+    print("printing")
+    if i == 2: # if i is 2, the iteration is skipped
+        continue
+    print(i)
+
+#pass
+l = [1,7,8]
+for item in l:
+    pass        # without pass, the program will throw an error
